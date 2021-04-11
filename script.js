70c7c213860b3ff53163a85746bb6d8a3a4558be
prguevarra <prguevarra@up.edu.ph>
2021-04-11 12:14:40 +0800

    Change computerPlay into function expression

--- script.js
+++ script.js
@@ -1,67 +1,80 @@
-//Welcome the user
+// Welcome the user and ask to play the game.
 console.log(`Hey there user! Are you ready to play the game? if so, type in %cgame()`, "color:white; background:red; style:bold;");
 
-// Generate random computer input
-function computerPlay() {
-    const shapes = ['rock', 'paper', 'scissors'];
-    const random = Math.floor(Math.random()* shapes.length);
-    return(shapes[random]);
+// Generate random computer answers.
+const computerPlay = function() {
+    const randomAnswer = Math.floor(Math.random()* 4);
+    if (randomAnswer === 2) {
+        computerAnswer = "ROCK"
+    } else if (randomAnswer === 1 || randomAnswer === 3){
+        computerAnswer = "PAPER"
+    } else {
+        computerAnswer = "SCISSORS"
+    }
+    return computerAnswer;
 }
-const computerSelection = computerPlay().toUpperCase();
-// console.log(computerSelection);
 
-// Ask for user input
+// Ask for user for input.
 function playerPlay() {
     const playerAnswer = prompt("ROCK, PAPER OR SCISSORS?").toUpperCase();
     return playerAnswer;
 }
 
-// play game 5 times
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    for (i=0; i<=4; i++) {
-        playerSelection = playerPlay();
-        playround(playerSelection, computerSelection);
-        if (playround(playerSelection, computerSelection) === "You lose!") {
-            computerScore += 1;
-            } else if (playround(playerSelection, computerSelection) === "You win!") {
-                playerScore += 1;
-            } else {
-                computerScore += 0;
-                playerScore += 0;
-            }
-    }
-    console.log(`You scored %c${playerScore}, while computer scored %c${computerScore}.`, "style: bold;");
-      if (playerScore >= 3) {
-            console.log("%cYOU WIN THE GAME!", "background:yellow; style:bold;");
-        } else if (playerScore < 3){
-            console.log("%cYOU LOSE THE GAME!", "color:white; background:red; style:bold;");
-        } else {
-            console.log("%cIT'S A DRAW!", "color:white; background:blue; style:bold;");
-        }
-    return console.log("Ready for another game? Type in %cgame()", "color:white; background:red; style:bold;");
-}
-
-// Single round of the game
+// Play a single round of the game
 function playround(playerSelection, computerSelection) {
     if (computerSelection == playerSelection) {
-        console.log("It's a draw!");
+        return "It's a draw";
     } else if 
              ((playerSelection == "PAPER" && computerSelection == "ROCK") ||
               (playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
               (playerSelection == "SCISSORS" && computerSelection == "PAPER"))
     {
-        console.log("You win!!");
         return "You win!";
     } else if
             ((playerSelection == "PAPER" && computerSelection == "SCISSORS") ||
              (playerSelection == "ROCK" && computerSelection == "PAPER") ||
              (playerSelection == "SCISSORS" && computerSelection == "ROCK"))
     {
-        console.log("You lose!");
         return "You lose!";
     } else { 
-        console.log("Hey! That's invalid!");
+        return "Invalid!";
     }
+}
+
+// Play game 5 consecutive times and print the overall verdict
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    let drawScore = 0;
+    let invalidScore = 0;
+    for (i=0; i<5; i++) {
+        computerSelection = computerPlay();
+        playerSelection = playerPlay();
+        playround(playerSelection, computerSelection);
+        if (playround(playerSelection, computerSelection) === "You lose!") {
+            console.log("You lose!");
+            computerScore += 1;
+        } else if (playround(playerSelection, computerSelection) === "You win!") {
+            console.log("You win!!");
+            playerScore += 1;
+        } else if (playround(playerSelection, computerSelection) === "It's a draw") {
+            console.log("It's a draw!")
+            drawScore += 1;
+        } else if (playround(playerSelection, computerSelection) === "Invalid!") {
+            console.log("Hey! That's invalid!");
+            invalidScore += 1;
+        }
+        console.log(`%cComputer answered ${computerSelection}`, "style:italic");
+
+    }
+        if (playerScore > computerScore) {
+            console.log("%cHOORAY! YOU WIN THE GAME!", "background:yellow; style:bold;");
+        } else if (playerScore < computerScore){
+            console.log("%cSORRY, YOU LOSE THE GAME!", "color:white; background:red; style:bold;");
+        } else {
+            console.log("%cOOH! IT'S A DRAW!", "color:white; background:blue; style:bold;");
+        }
+    console.log(`%cYou scored [${playerScore}] and computer scored [${computerScore}]
+        while [${drawScore}] game(s) is/are draw and you entered [${invalidScore}] invalid entry/entries!%c`, "background:green, style: bold;");
+    return console.log("Would you like to play again? Type in %cgame()", "color:white; background:red; style:bold;");
 }
