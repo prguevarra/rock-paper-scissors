9575c45d5259d925cfcd0cddad21b789c5bf83c4
prguevarra <prguevarra@up.edu.ph>
2021-05-06 22:51:43 +0800

    Create new function for text visibility

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+
 // query selectors for the divs
+const container = document.getElementById('#weapons');
 const gameResult = document.querySelector('.results');
 const weaponsFired = document.querySelector('.round-weapons');
 const scoreBoard = document.querySelector('.score-board');
 const finalVerdict = document.querySelector('.final-verdict');
 const weapons = document.querySelectorAll('.weapon');
+const playAgain = document.querySelectorAll('play-again');
+const theDivs = document.querySelectorAll('the-divs');
+
+// declare variables
+const MAX_ROUNDS = 5
+let playerScore = 0;
+let computerScore = 0;
 
 // add event listeners for each choice
 weapons.forEach((weapon, index) => {
@@ -30,20 +39,19 @@ function playerPlay(e) {
     playround(playerSelection, computerSelection);
 }
 
-let playerScore = 0;
-let computerScore = 0;
 
 // plays 1 round of the game and display result
 function playround(playerSelection, computerSelection) {
-    weaponsFired.textContent = `You chose ${playerSelection} while computer chose ${computerSelection}`;
+    weaponsFired.textContent = `You chose ${playerSelection} while miming chose ${computerSelection}`;
+    hideOverallResult();
 
-    if (computerSelection == playerSelection) {
+    if (computerSelection === playerSelection) {
         gameResult.textContent = "Oh! It's a draw.";
         gameResult.style.cssText = 'color: blue';
     } else if 
-             ((playerSelection == "paper" && computerSelection == "rock") ||
-              (playerSelection == "rock" && computerSelection == "scissors") ||
-              (playerSelection == "scissors" && computerSelection == "paper"))
+             ((playerSelection === "paper" && computerSelection === "rock") ||
+              (playerSelection === "rock" && computerSelection === "scissors") ||
+              (playerSelection === "scissors" && computerSelection === "paper"))
     {
         gameResult.textContent = "Nice! one point for you!";
         gameResult.style.cssText = 'color: green';
@@ -67,22 +75,47 @@ function playround(playerSelection, computerSelection) {
 // Play game 5 consecutive times and print the overall verdict
 function playVerdict(){ 
     scoreBoard.textContent = `YOUR SCORE: ${playerScore} MIMING SCORE: ${computerScore}`;
+    
 
-    if (playerScore === 5 || computerScore === 5) {
-        cleanUp();
-        if (playerScore === 5) {
+    if (playerScore === MAX_ROUNDS || computerScore === MAX_ROUNDS) {
+        showOverallResult();
+        if (playerScore === MAX_ROUNDS) {
             finalVerdict.textContent = "HOORAY! YOU WIN THE GAME!";
-            finalVerdict.style.cssText = 'color:yellow;';            
-        } else if (computerScore === 5) {
+            finalVerdict.style.cssText = 'color:yellow;';      
+        } else if (computerScore === MAX_ROUNDS) { 
+
             finalVerdict.textContent = "SORRY, YOU LOSE THE GAME!";
             finalVerdict.style.cssText = 'color:red;';
         }
-        alert('Play again?');
-        location.reload();
+        reSetGame();
+        playAgain(); 
     }
 }
 
-function cleanUp() {
-    gameResult.remove();
-    weaponsFired.remove();
+function hideOverallResult() {
+    gameResult.style.visibility = "visible";
+    weaponsFired.style.visibility = "visible";
+    finalVerdict.style.visibility = "hidden";
 }
+
+function showOverallResult() {
+    gameResult.style.visibility = "hidden";
+    weaponsFired.style.visibility = "hidden";
+    finalVerdict.style.visibility = "visible";
+
+}
+
+function reSetGame() {
+    playerScore = 0;
+    computerScore = 0;
+}
+
+// function playAgain() {
+    
+// }
+
+    // const playAgainButton = document.createElement('div');
+    // playAgainButton.classList.add("another-button");
+    // playAgainButton.textContent = "Play again?";
+    // playAgain.appendChild(playAgainButton);
+    // playAgain.textContent = "Another Round?";
