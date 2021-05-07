27500afef9db5e18823d06c612690f82e50b4103
prguevarra <prguevarra@up.edu.ph>
2021-05-07 12:14:01 +0800

    Add play button to reset game

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // query selectors for the divs
-const container = document.getElementById('#weapons');
+const container = document.querySelector('main');
 const gameResult = document.querySelector('.results');
 const weaponsFired = document.querySelector('.round-weapons');
 const scoreBoard = document.querySelector('.score-board');
@@ -9,12 +9,14 @@ const weapons = document.querySelectorAll('.weapon');
 const playAgain = document.querySelectorAll('play-again');
 const theDivs = document.querySelectorAll('the-divs');
 
+
 // declare variables
 const MAX_ROUNDS = 5
 let playerScore = 0;
 let computerScore = 0;
 
 // add event listeners for each choice
+
 weapons.forEach((weapon, index) => {
     weapon.addEventListener('click', playerPlay);
 });
@@ -39,11 +41,10 @@ function playerPlay(e) {
     playround(playerSelection, computerSelection);
 }
 
-
 // plays 1 round of the game and display result
 function playround(playerSelection, computerSelection) {
     weaponsFired.textContent = `You chose ${playerSelection} while miming chose ${computerSelection}`;
-    hideOverallResult();
+    showBoard();
 
     if (computerSelection === playerSelection) {
         gameResult.textContent = "Oh! It's a draw.";
@@ -71,11 +72,9 @@ function playround(playerSelection, computerSelection) {
     playVerdict(playerScore, computerScore);
 }
 
-
 // Play game 5 consecutive times and print the overall verdict
 function playVerdict(){ 
     scoreBoard.textContent = `YOUR SCORE: ${playerScore} MIMING SCORE: ${computerScore}`;
-    
 
     if (playerScore === MAX_ROUNDS || computerScore === MAX_ROUNDS) {
         showOverallResult();
@@ -88,34 +87,49 @@ function playVerdict(){
             finalVerdict.style.cssText = 'color:red;';
         }
         reSetGame();
-        playAgain(); 
     }
 }
 
-function hideOverallResult() {
+// play another round of game upon clicking "play again" button
+function reSetGame() {
+    playerScore = 0;
+    computerScore = 0; 
+    
+    const resetButton = document.createElement('button');
+    resetButton.classList.add('play-again-button');
+    resetButton.textContent = "PLAY AGAIN";
+    finalVerdict.appendChild(resetButton);
+
+    resetButton.addEventListener('click', anotherRound);
+}
+
+function anotherRound(e) {
+    clearBoard();
+    
+    container.classList.remove('endgame');
+}
+
+//  helper functions
+function clearBoard() {
+    gameResult.style.visibility = "hidden";
+    weaponsFired.style.visibility = "hidden";
+    finalVerdict.style.visibility = "hidden";
+    scoreBoard.style.visibility = "hidden";
+}
+
+function showBoard() {
     gameResult.style.visibility = "visible";
     weaponsFired.style.visibility = "visible";
-    finalVerdict.style.visibility = "hidden";
+    scoreBoard.style.visibility = "visible";
 }
 
 function showOverallResult() {
+    freezeImages();
     gameResult.style.visibility = "hidden";
     weaponsFired.style.visibility = "hidden";
     finalVerdict.style.visibility = "visible";
-
 }
 
-function reSetGame() {
-    playerScore = 0;
-    computerScore = 0;
+function freezeImages() {
+    container.classList.add('endgame');    
 }
-
-// function playAgain() {
-    
-// }
-
-    // const playAgainButton = document.createElement('div');
-    // playAgainButton.classList.add("another-button");
-    // playAgainButton.textContent = "Play again?";
-    // playAgain.appendChild(playAgainButton);
-    // playAgain.textContent = "Another Round?";
